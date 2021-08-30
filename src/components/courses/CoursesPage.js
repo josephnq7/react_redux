809105588e13499be07ce30943c7a54297f40ac2
Joseph Nguyen <joseph.nguyen@soaren-management.com>
2021-08-30 14:30:06 -0700

    mapDispatchToProps: bindActionCreators

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import * as courseActions from "../../redux/actions/courseActions";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
 
 class CoursesPage extends React.Component {
   state = {
@@ -19,7 +20,8 @@ class CoursesPage extends React.Component {
     event.preventDefault();
     // alert(this.state.course.title);
     // this.props.dispatch(courseActions.createCourse(this.state.course));
-    this.props.createCourse(this.state.course);
+    // this.props.createCourse(this.state.course);
+    this.props.actions.createCourse(this.state.course);
   };
 
   render() {
@@ -43,7 +45,8 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   //   dispatch: PropTypes.func.isRequired,
-  createCourse: PropTypes.func.isRequired,
+  //   createCourse: PropTypes.func.isRequired,
+  actions: PropTypes.object.isRequired,
   courses: PropTypes.array.isRequired,
 };
 
@@ -56,7 +59,7 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    createCourse: (course) => dispatch(courseActions.createCourse(course)),
+    actions: bindActionCreators(courseActions, dispatch),
   };
 }
 
